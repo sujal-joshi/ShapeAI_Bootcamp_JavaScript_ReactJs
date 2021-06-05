b095428d1d42f3db74fcd13876abf8766b73640c
sujal-joshi <85385769+sujal-joshi@users.noreply.github.com>
2021-06-05 12:22:21 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>McLaren</h1>
+      <p>
+        McLaren Racing Limited is a British motor racing team based at the
+        McLaren Technology Centre in Woking, Surrey, England.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
